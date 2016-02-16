f94384a741d504523b01d23441222cbe64780ad3
johnvehr <johnvehr@yahoo.com>
2016-02-15 20:21:32 -0700

    save data function

--- client/app/application/application.controller.js
+++ client/app/application/application.controller.js
@@ -6,4 +6,13 @@ angular.module('empApplicationGenApp')
     $scope.formStatus = false;
     $scope.appForm = {}
 
+    $scope.saveData = function(){
+      $scope.formStatus = false;
+      $scope.post('/api/applications/write', $scope.appForm).success(function(res){
+        console.log("Success")
+      }).error(function(){
+        console.log("Error on callback from server")
+      })
+    }
+
   });
